a0c6f42cd0110328cd41ed477b20578bbcceec9d
Suyesh69420 <suyeshghimire5@gmail.com>
2025-08-10 15:03:31 +0545

    Update prog_<6>.c
    Completed min & max heap build and doc

--- prog_<6>.c
+++ prog_<6>.c
@@ -1,3 +1,4 @@
+/* prog_6.c - Commit 3 (final) */
 #include <stdio.h>
 
 void heapify(int arr[], int n, int i, int isMax){
@@ -7,19 +8,24 @@ void heapify(int arr[], int n, int i, int isMax){
         if(r<n && arr[r]>arr[ext]) ext=r;
     } else {
         if(l<n && arr[l]<arr[ext]) ext=l;
-         if(r<n && arr[r]<arr[ext]) ext=r;
+        if(r<n && arr[r]<arr[ext]) ext=r;
     }
     if(ext!=i){ int t=arr[i]; arr[i]=arr[ext]; arr[ext]=t; heapify(arr,n,ext,isMax); }
 }
+
 void forgeHeap(int arr[], int n, int isMax){
     for(int i=n/2-1;i>=0;--i) heapify(arr,n,i,isMax);
 }
+
+void printArr(int a[], int n){ for(int i=0;i<n;++i) printf("%d ", a[i]); printf("\n"); }
+
 int main(){
     int a[]={4,10,3,5,1}, n=5;
-    int b[5]; for(int i=0;i<n;++i) b[i]=a[i];
-    forgeHeap(b,n,1); printf("MaxHeap: "); 
-  for(int i=0;i<n;++i) printf("%d ", b[i]); printf("\n");
-    forgeHeap(a,n,0); printf("MinHeap: "); 
-  for(int i=0;i<n;++i) printf("%d ", a[i]); printf("\n");
+    int maxArr[5]; int minArr[5];
+    for(int i=0;i<n;++i){ maxArr[i]=a[i]; minArr[i]=a[i]; }
+    forgeHeap(maxArr,n,1);
+    forgeHeap(minArr,n,0);
+    printf("Max Heap: "); printArr(maxArr,n);
+    printf("Min Heap: "); printArr(minArr,n);
     return 0;
 }
