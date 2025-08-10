025d7c383dd3dcb02138be0a437f57ddb49fac9a
Suyesh69420 <suyeshghimire5@gmail.com>
2025-08-10 14:46:19 +0545

    Update prog_<5>.c

--- prog_<5>.c
+++ prog_<5>.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+
+#define V 5
+
+void mapAdj(int adj[V][V]); /* create or demo graph */
+void runBFS(int adj[V][V], int start);
+void runDFS(int adj[V][V], int start);
+
+int main() { printf("Q5 skeleton ready.\n"); return 0; }
 
