2378990d735fb737b92345dd620465c5af2b29bb
Suyesh69420 <suyeshghimire5@gmail.com>
2025-08-10 13:57:59 +0545

    Update prog_<3>.c

--- prog_<3>.c
+++ prog_<3>.c
@@ -3,10 +3,21 @@
 
 struct Node { int val; struct Node *next; };
 
-void pushNode(struct Node **head, int v);
-void walkBack(struct Node *head); /* reverse traversal */
+void pushNode(struct Node **head, int v) {
+    struct Node *n = malloc(sizeof(*n)); n->val=v; n->next=*head; *head=n;
+}
+
+void walkBack(struct Node *head) {
+    if (!head) return;
+    walkBack(head->next);
+    printf("%d ", head->val);
+}
 
 int main() {
-    printf("Q3 skeleton ready.\n");
+    struct Node *h = NULL;
+    pushNode(&h, 10); pushNode(&h, 20); pushNode(&h, 30);
+    printf("Reverse traversal: ");
+    walkBack(h);
+    printf("\n");
     return 0;
 }
